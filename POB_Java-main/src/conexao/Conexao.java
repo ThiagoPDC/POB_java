aeb01c02c54c161d440a5ae7baf80d9e640bb4c4
Thiago Pavan Dalla Costa <129401426+ThiagoPDC@users.noreply.github.com>
2023-08-28 12:33:53 -0300

    POB_JAVA st commit

---
+++ POB_Java-main/src/conexao/Conexao.java
@@ -0,0 +1,16 @@
+package conexao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class Conexao {
+    public static Connection getConexao(){
+        try {
+            return DriverManager.getConnection("jdbc:mysql://localhost:3306/aula2408a?serverTimezone=UTC", 
+                    "root", "1423");
+        } catch (Exception e) {
+            System.out.println("Erro ao conectar: " + e.getMessage());
+        }
+        return null;
+    }
+}
